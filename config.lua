75596373eabeefce91fdc505594c5b94400fcf95
GenixScripts <frstudios24@gmail.com>
2025-02-14 21:37:47 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,66 @@
+Config = {}
+
+Config.NPCLocations = {
+    {
+        coords = vector4(-230.12, 6445.09, 31.2, 136.61),
+        model = "a_m_y_business_03",
+        scenario = "WORLD_HUMAN_STAND_MOBILE"
+    }
+}
+
+Config.Items = {
+    weapons = {
+        {
+            name = "weapon_pistol",
+            label = "Pistol",
+            price = 650000,
+            description = ""
+        },
+        {
+            name = "weapon_smg",
+            label = "SMG",
+            price = 850000,
+            description = ""
+        },
+        {
+            name = "WEAPON_APPISTOL",
+            label = "APP Pistol",
+            price = 850000,
+            description = ""
+        },
+        {
+            name = "WEAPON_ASSAULTRIFLE",
+            label = "Aussault Rifle",
+            price = 850000,
+            description = ""
+        }
+    },
+    drugs = {
+        {
+            name = "weed_white-widow",
+            label = "White Widow",
+            price = 450000,
+            description = ""
+        },
+
+    },
+    tools = {
+        {
+            name = "lockpick",
+            label = "Lockpick",
+            price = 25000,
+            description = ""
+        },
+
+    },
+ --   others = {
+ --       {
+ --           name = "phone",
+ --           label = "phone",
+ --           price = 40,
+ --           description = ""
+ --       },
+
+ --   }
+}
+
